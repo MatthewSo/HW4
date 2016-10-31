6791225f44ecf8953a78855b45801214a65b1bab
MatthewSo <mso@stuy.edu>
2016-10-31 10:47:21 -0400

    done

--- stat.c
+++ stat.c
@@ -8,10 +8,22 @@ int main() {
 
   
   struct stat temp;
-  stat("file.txt", &temp);
+  stat("file2.txt", &temp);
   printf("Info for file.txt\n");
   printf("Size: %lu bytes\n", temp.st_size);
   printf("Permissions: %o\n", temp.st_mode);
   printf("Time of last accessed: %s", ctime(&temp.st_atim));
-
+  long m = temp.st_size;
+  if (m < 1000){
+    printf( "Size: %f B\n", m);
+  }
+  else if (m < 1000000){
+    printf( "Size: %f KB\n", m/1000.0);
+  }
+  else if (m < 1000000000){
+    printf( "Size: %f MB\n", m/1000000.0);
+  }
+  else{
+    printf( "Size: %f MB\n", m/1000000000.0);}
+  
 }
