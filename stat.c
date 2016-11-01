6e991d1ec5a3c2f57f673b21c1d98106000e8527
MatthewSo <mso@stuy.edu>
2016-10-31 20:25:01 -0400

    added

--- stat.c
+++ stat.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <time.h>
 
 
@@ -25,5 +27,51 @@ int main() {
   }
   else{
     printf( "Human Size: %f GB\n", m/1000000000.0);}
+
+  printf("Human File Permissions: \t");
+  if (fileStat.st_mode & S_IRUSR) {
+    printf("r");}
+  else{
+    printf("-");}
+
+  if (fileStat.st_mode & S_IWUSR) {
+    printf("w");}
+  else{
+    printf("-");}
+
+  if (fileStat.st_mode & S_IXUSR) {
+    printf("x");}
+  else{
+    printf("-");}
+
+      if (fileStat.st_mode & S_IRGRP) {
+    printf("r");}
+  else{
+    printf("-");}
+
+  if (fileStat.st_mode & S_IWGRP) {
+    printf("w");}
+  else{
+    printf("-");}
+
+  if (fileStat.st_mode & S_IXGRP){
+    printf("x");}
+  else{
+    printf("-");}
+
+      if (fileStat.st_mode & S_IROTH) {
+    printf("r");}
+  else{
+    printf("-");}
+
+      if  (fileStat.st_mode & S_IWOTH){
+    printf("w");}
+  else{
+    printf("-");}
+
+  if (fileStat.st_mode & S_IXOTH) {
+    printf("x");}
+  else{
+    printf("-");}
   
 }
